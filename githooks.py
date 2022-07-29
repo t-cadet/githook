55f660249dab7f61add60741620d898fb1adad02
Tristan Cadet <tristan.cadet@externe.bnpparibas.com>
2022-07-29 16:21:05 +0200

    new: a

--- githooks.py
+++ githooks.py
@@ -1,41 +1,56 @@
+#!/usr/bin/python3
+
+import os, getpass
 import abc
+from colorama import Fore, Style
 import io
+import subprocess
 import sys
 from dataclasses import dataclass
-from typing import Callable, List, Optional
+from typing import Callable, List, Optional, Tuple
 
 
 # FIXME: this class is only used as a namespace
 class Utils(abc.ABC):
     @staticmethod
-    def bail(msg: str):
-        pass
+    def cprint(msg: str, file=sys.stdout, color="", style=""):
+        for line in msg.splitlines():
+            print(f"{color}{style}{line}{Style.RESET_ALL}", file=file)
+
+    # always exit
+    @staticmethod
+    def bail(msg: str, exit_code = 1, color="", style=""):
+        Utils.cprint(msg, color=color, style=style, file=sys.stderr)
+        exit(exit_code)
 
     @staticmethod
-    def non_bail_exec() -> (str, str, bool):
-        return ("", "", True)
+    def non_bail_exec(cmd: str) -> subprocess.CompletedProcess:
+        return subprocess.run(cmd, shell=True, capture_output=True, text=True)
 
-    # FIXME: flesh out fn
+    # may exit
     @staticmethod
-    def exec() -> str:
-        pass
-
-    # FIXME: flesh out fn
+    def exec(cmd: str) -> str:
+        completed_process = Utils.non_bail_exec(cmd)
+        if completed_process.returncode != 0:
+            Utils.bail(f"Failed to push: cmd `{cmd}` failed: {completed_process.stderr.rstrip()}", color=Fore.MAGENTA)
+        return completed_process.stdout
+    
+    # may exit when pred may exit
     @staticmethod
-    def check(pred: Callable[[], bool], description: str, color=None) -> bool:
-        print(f"    {description}", file=sys.stderr)
+    def check(pred: Callable[[], bool], description: str) -> bool:
+        Utils.cprint(f"    {description}", file=sys.stderr, color=Fore.YELLOW)
         ok = pred()
         if ok:
-            print("      OK")
+            Utils.cprint("      OK", file=sys.stderr, color=Fore.MAGENTA)
         else:
-            print("      KO")
+            Utils.cprint("      KO", file=sys.stderr, color=Fore.MAGENTA)
         return ok
 
     # FIXME: handle panic
     # FIXME: flesh out fn
     @staticmethod
     def commit_hashes_between(fst: str, lst: str) -> List[str]:
-        return [fst]
+        return Utils.exec(f"'git rev-list --reverse --topo-order {fst}..{lst}'").splitlines()
 
 
 @dataclass
@@ -82,12 +97,14 @@ class PreReceiveContext:
     def from_reader(r: io.TextIOBase) -> "PreReceiveContext":
         ctx = PreReceiveContext([])
 
-        while line := r.readline():
+        line = r.readline()
+        while line:
             [old_value, new_value, ref_name] = line.split()
             commit_hashes = Utils.commit_hashes_between(old_value, new_value)
             commits = [Commit.from_hash(c_hash) for c_hash in commit_hashes]
             ref = Ref(ref_name, commits)
             ctx.refs.append(ref)
+            line = r.readline()
 
         return ctx
 
@@ -95,7 +112,7 @@ class PreReceiveContext:
 def rust_hook():
     def run_tests(commit: Commit) -> bool:
         (_, _, ok) = Utils.non_bail_exec()
-        return True
+        return ok
 
     def check_fmt_on_lst_commit(ref: Ref) -> bool:
         files_to_fmt = set()
@@ -107,16 +124,17 @@ def rust_hook():
         return ok
 
     ctx = PreReceiveContext.from_stdin()
+    print(ctx, flush=True)
     for ref in ctx.refs:
-        if ref.name == "refs/head/master":
+        if ref.name == "refs/heads/master":
 
-            print("rustfmt --check")
+            print("rustfmt --check", file=sys.stderr)
             if not Utils.check(
                 lambda ref=ref: check_fmt_on_lst_commit(ref), f"{ref.commits[-1].hash}"
             ):
                 Utils.bail("fmt failed")
 
-            print("cargo test --release")
+            print("cargo test --release", file=sys.stderr)
             for commit in ref.commits:
                 if not Utils.check(
                     lambda commit=commit: run_tests(commit), f"{commit.hash}"
@@ -128,15 +146,20 @@ def test1():
     mock_input = io.StringIO(
         "old_value new_value ref_name\nold_value2\tnew_value2  ref_name2"
     )
-    print(f"{PreReceiveContext.from_reader(mock_input)=}")
+    print(f"context mock input={PreReceiveContext.from_reader(mock_input)}")
 
 
 def main():
     print("main")
-    print(f"{PreReceiveContext.from_stdin()=}")
+    print(f"context from stdin={PreReceiveContext.from_stdin()}")
 
 
 if __name__ == "__main__":
     # main()
     # test1()
+    print("Entering pre-receive hook", flush=True)
+    print("Env thinks the user is [%s]" % (os.getlogin()), flush=True)
+    print("Effective user is [%s]" % (getpass.getuser()), flush=True)
+
     rust_hook()
+    exit(1)
